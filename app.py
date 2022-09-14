96198ab8780f17f566c9cc7f6d6fb0775c5faf2d
jwitherell24 <witherellj24@gmail.com>
2022-09-14 09:24:58 -0400

    able to view a book

--- app.py
+++ app.py
@@ -53,8 +53,8 @@ def clean_price(price_str):
     try:
         price_float = float(price_str)
     except ValueError:
-                input("""
-              \n****** Prie ERROR ******
+        input("""
+              \n****** PRICE ERROR ******
               \rThe price should be a number without a currency symbol.
               \rEx. 10.99
               \rPress enter to try again.
@@ -62,6 +62,27 @@ def clean_price(price_str):
     else:
         return int(price_float * 100)
     
+    
+def clean_id(id_str, options):
+    try:
+        book_id = int(id_str)
+    except ValueError:
+        input("""
+              \n****** ID ERROR ******
+              \rThe id should be a number.
+              \rPress enter to try again.
+              ***************************""")
+        return
+    else:
+        if book_id in options:
+            return book_id 
+        else:
+            input(f"""
+              \n****** ID ERROR ******
+              \rOptions: {options}
+              \rPress enter to try again.
+              ***************************""")
+            return
     
     
 def add_csv():
@@ -104,13 +125,28 @@ def app():
             print("Book Added!")
             time.sleep(1.5)
         elif choice == "2":
-            # view books
             for book in session.query(Book):
                 print(f"{book.id} | {book.title} | {book.author}")
             input("\nPress enter to return to the main menu.")
         elif choice == "3":
             # search book
-            pass
+            id_options = []
+            for book in session.query(Book):
+                id_options.append(book.id)
+            id_error = True
+            while id_error:
+                id_choice = input(f"""
+                    \nId Options: {id_options}
+                    \rBook id:  """)
+                id_choice = clean_id(id_choice, id_options)
+                if type(id_choice) == int:
+                    id_error = False
+            the_book = session.query(Book).filter(Book.id==id_choice).first()
+            print(f"""
+                  \n{the_book.title} by {the_book.author}
+                  \rPublished: {the_book.published_date}
+                  \rPrice: ${the_book.price / 100}""")
+            input("\nPress enter to return to the main menu")
         elif choice == "4":
             # analysis
             pass
